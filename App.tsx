4dccb88cdf2baa26073d7b62907c3bb650de3d45
Alexis Castellanos <castellanos.alexis@hotmail.com>
2023-10-12 11:58:29 -0600

    setup

---
+++ App.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+
+function App(): JSX.Element {
+  return (
+    <SafeAreaView style={styles.container}>
+      <View>
+        <Text style={styles.text}>Hello World</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  text: {
+    color: 'black',
+  },
+});
+
+export default App;
