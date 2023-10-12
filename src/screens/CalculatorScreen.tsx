c7bc995843d5a4e8be05cfe8454bf97b39f2e8bb
Alexis Castellanos <castellanos.alexis@hotmail.com>
2023-10-12 14:30:41 -0600

    finish buttons and styles

--- src/screens/CalculatorScreen.tsx
+++ src/screens/CalculatorScreen.tsx
@@ -16,6 +16,33 @@ export const CalculatorScreen = () => {
         <ButtonCalc text="del" color="#9b9b9b" />
         <ButtonCalc text="/" color="#ff9427" />
       </View>
+
+      <View style={styles.row}>
+        <ButtonCalc text="7" />
+        <ButtonCalc text="8" />
+        <ButtonCalc text="9" />
+        <ButtonCalc text="x" color="#ff9427" />
+      </View>
+
+      <View style={styles.row}>
+        <ButtonCalc text="4" />
+        <ButtonCalc text="5" />
+        <ButtonCalc text="6" />
+        <ButtonCalc text="-" color="#ff9427" />
+      </View>
+
+      <View style={styles.row}>
+        <ButtonCalc text="1" />
+        <ButtonCalc text="2" />
+        <ButtonCalc text="3" />
+        <ButtonCalc text="+" color="#ff9427" />
+      </View>
+
+      <View style={styles.row}>
+        <ButtonCalc text="0" wide />
+        <ButtonCalc text="." />
+        <ButtonCalc text="=" color="#ff9427" />
+      </View>
     </View>
   );
 };
